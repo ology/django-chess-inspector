e0a5cde25578c6ddac27aeeb5bba2ca526017a44
Gene Boggs <gene@ology.net>
2024-12-30 23:16:15 -0800

    CSRF_TRUSTED_ORIGINS

--- chess-inspector/inspector/settings.py
+++ chess-inspector/inspector/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = 'django-insecure-@n-40n$ke4!#28l4ts0nx41#fh2e5&bwpiaul%-6(i2+55^zyq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.100.50', '68.67.110.36']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.100.50']
 
+CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1:8000', 'http://192.168.100.50:8080']
 
 # Application definition
 
